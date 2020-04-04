e0a7baccfb74d3279d5196bba33548b193dbf066
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-04-04 08:47:28 +0300

    Latest cutter-deps build with xkb.

--- scripts/fetch_deps.sh
+++ scripts/fetch_deps.sh
@@ -4,15 +4,15 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 mkdir -p cutter-deps && cd cutter-deps
 
 LINUX_FILE="cutter-deps-linux.tar.gz"
-LINUX_MD5=dceee1b643d3891bcfe2c06be59ff267
+LINUX_MD5=99447697f6a9d39ef98b61287cdbc149
 LINUX_URL=https://github.com/radareorg/cutter-deps/releases/download/v11/cutter-deps-linux.tar.gz
 
 MACOS_FILE="cutter-deps-macos.tar.gz"
-MACOS_MD5=8f55dfeb8fb9ed98f7ccc21fbc134582
+MACOS_MD5=5b1e44f9d07de2be47023270091fcfa5
 MACOS_URL=https://github.com/radareorg/cutter-deps/releases/download/v11/cutter-deps-macos.tar.gz
 
 WIN_FILE="cutter-deps-win.tar.gz"
-WIN_MD5=793549bb6fd819e5e62bf7121dfc281b
+WIN_MD5=b86947938eaf4bc46eab79e53c114778
 WIN_URL=https://github.com/radareorg/cutter-deps/releases/download/v11/cutter-deps-win.tar.gz
 
 if [ "$OS" == "Windows_NT" ]; then
